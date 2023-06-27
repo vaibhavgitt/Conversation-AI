738dd7e44498560e56089880dce7d19dcfe94b1e
Vaibhav <vaibhavigawe1@gmail.com>
2023-06-27 22:10:49 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from caching import Cache
+
+@st.cache(max_entries=100, ttl=3600, suppress_st_warning=True)
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+  
+  
+def get_vectorstores(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriver(),
+        memory=memory
+    )
+    return conversation_chain
+    
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":Books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+    
+    
+
+    with st.sidebar:
+        st.subheader("Your Documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+                
+                #create vector store
+                vectorstore = get_vectorstores(text_chunks)
+                
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore) 
+                
+                  
+    #st.session_state.conversation
+
+if __name__ == "__main__":
+    main()
+
